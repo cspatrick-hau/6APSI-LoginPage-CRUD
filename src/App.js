5b00a6cea31d912a238ee0e40e19d761c03659a0
DESKTOP-H91VNKQ\Patrick <ptsigua@student.hau.edu.ph>
2025-07-25 21:25:24 +0800

    LandingPage.js Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './LoginPage';
+import LandingPage from './LandingPage';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/landing" element={<LandingPage />} />
       </Routes>
     </Router>
   );
